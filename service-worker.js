0c9ce2c5d06e39288d3824891769e1f19809f771
lawhite-bank-demo <lawihtenba@gmail.com>
2026-03-06 19:42:52 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "crypto-wallet-cache-v1"
+
+const urlsToCache = [
+  "/",
+  "index.html",
+  "dashboard.html",
+  "admin.html",
+  "admin-login.html",
+  "style.css",
+  "app.js"
+]
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache)
+    })
+  )
+
+})
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request)
+    })
+  )
+
+})
